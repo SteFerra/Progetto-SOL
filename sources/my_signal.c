0cec2df76ce4c191f7ea123210abdb5c9afa1eb4
SteFerra <stefanoferralis@gmail.com>
2024-04-06 19:30:47 +0200

    Aggiunto il controllo per la gestione dei segnali

---
+++ sources/my_signal.c
@@ -0,0 +1,41 @@
+#include <pthread.h>
+#include <signal.h>
+#include <string.h>
+#include <stdio.h>
+#include "my_signal.h"
+
+void set_signal_mask(){
+    sigset_t set;
+    sigemptyset(&set);
+    sigaddset(&set, SIGHUP);
+    sigaddset(&set, SIGINT);
+    sigaddset(&set, SIGQUIT);
+    sigaddset(&set, SIGTERM);
+    sigaddset(&set, SIGUSR1);
+    sigaddset(&set, SIGUSR2);
+    pthread_sigmask(SIG_SETMASK, &set, NULL);
+}
+
+void register_handlers(){
+    struct sigaction sa;
+    memset(&sa, 0, sizeof(sa));
+    sa.sa_handler = termination_handler;
+    sigaction(SIGHUP, &sa, NULL);
+    sigaction(SIGINT, &sa, NULL);
+    sigaction(SIGQUIT, &sa, NULL);
+    sigaction(SIGTERM, &sa, NULL);
+    //TO-DO aggiungere SIGUSR1 e SIGUSR2
+
+}
+
+void unmask(){
+    interrupted = 0;
+    sigset_t set;
+    sigemptyset(&set);
+    pthread_sigmask(SIG_SETMASK, &set, NULL);
+}
+
+void termination_handler(int signum){
+    interrupted = 1;
+}
+
