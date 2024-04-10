ba299353defbd0e12b191401989fd74924965269
SteFerra <stefanoferralis@gmail.com>
2024-04-10 21:50:37 +0200

    Completata la funzione get_options, adesso gestisce i casi in cui l'opzione -d è presente o meno.

--- includes/master.h
+++ includes/master.h
@@ -2,6 +2,13 @@
 #ifndef PROGETTO_MASTER_H
 #define PROGETTO_MASTER_H
 
+
+#include "linked_list.h"
+
 int master_worker(int argc, char *argv[]);
+
+void clear_all(linkedlist argfiles);
+
+void explore_dir(char *directory, linkedlist argfiles);
 
 #endif //PROGETTO_MASTER_H
--- sources/master.c
+++ sources/master.c
@@ -1,11 +1,13 @@
 #include <getopt.h>
+#include <sys/stat.h>
+#include <dirent.h>
 #include "master.h"
 #include "my_signal.h"
 #include "linked_list.h"
 #include "util.h"
 
 
-int get_options(int argc, char* argv[], int* nthread, int* qlen, int* delay, char *directory);
+int get_options(int argc, char* argv[], int* nthread, int* qlen, int* delay, char *directory, linkedlist argfiles);
 
 int master_worker(int argc, char *argv[]){
     interrupted = 0;
@@ -14,21 +16,21 @@ int master_worker(int argc, char *argv[]){
     int nthread, qlen, delay;
     char directory[MAX_PATH_LEN + 1];
     linkedlist argfiles;
-
-
+    init_list(&argfiles);
 
     //recupero gli argomenti passati al programma
-    if (ret = get_options(argc, argv, &nthread, &qlen, &delay, directory) != 0){
-        //TODO: pulire la memoria;
+    if (ret = get_options(argc, argv, &nthread, &qlen, &delay, directory, argfiles) != 0){
+        clear_all(argfiles);
         print_error("Errore nella lettura degli argomenti\n");
     }
+
 
     //sblocco i segnali
     unmask();
     return 0;
 }
 
-int get_options(int argc, char* argv[], int *nthread, int *qlen, int *delay, char *directory){
+int get_options(int argc, char* argv[], int *nthread, int *qlen, int *delay, char *directory, linkedlist argfiles){
     int opt;
     while((opt = getopt(argc, argv, "n:q:t:d:")) != -1){
         switch(opt){
@@ -56,7 +58,8 @@ int get_options(int argc, char* argv[], int *nthread, int *qlen, int *delay, cha
                     return -1;
                 }
                 strcpy(directory, optarg);
-                printf("directory: %s\n", directory);
+                printf("directory: %s\nProcedo ad esplorarla\n", directory);
+                explore_dir(directory, argfiles);
                 break;
             }
             default:
@@ -67,11 +70,68 @@ int get_options(int argc, char* argv[], int *nthread, int *qlen, int *delay, cha
     }
 
     // Processa gli argomenti rimanenti (i nomi dei file)
+    struct stat file_stat;
     for(int i = optind; i < argc; i++){
-        //TODO: aggiungere i file alla lista
+        //controllo se il file è un file regolare
+        if(stat(argv[i], &file_stat) != 0){
+            print_error("Errore nella lettura delle informazioni del file %s\n", argv[i]);
+            return -1;
+        }
+        if(!S_ISREG(file_stat.st_mode)){
+            print_error("Il file %s non è un file regolare\n", argv[i]);
+            return -1;
+        }
+        //aggiungo il file alla lista
+        if(add_file(argfiles, argv[i]) != 0){
+            print_error("Errore nell'aggiunta del file %s alla lista\n", argv[i]);
+            return -1;
+        }
     }
 
     return 0;
+}
+
+void explore_dir(char *directory, linkedlist argfiles){
+    DIR *dir;
+    struct dirent *entry;
+    struct stat file_stat;
+    char path[MAX_PATH_LEN + 1];
+    if((dir = opendir(directory)) == NULL){
+        print_error("Errore nell'apertura della directory %s\n", directory);
+        return;
+    }
+    while((entry = readdir(dir)) != NULL){
+        if(strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0){
+            continue;
+        }
+        if(strlen(directory) + strlen(entry->d_name) + 2 > MAX_PATH_LEN){
+            print_error("Path length for file %s in directory %s exceeds maximum allowed length\n", entry->d_name, directory);
+            continue;
+        }
+        if((snprintf(path, MAX_PATH_LEN, "%s/%s", directory, entry->d_name)) > MAX_PATH_LEN){
+            print_error("Errore nella creazione del path per il file %s\n", entry->d_name);
+            continue;
+        }
+        if(stat(path, &file_stat) != 0){
+            print_error("Errore nella lettura delle informazioni del file %s\n", path);
+            continue;
+        }
+        if(S_ISDIR(file_stat.st_mode)){
+            explore_dir(path, argfiles);
+        }
+        //se è un file regolare aggiungo il path del file alla lista
+        else if(S_ISREG(file_stat.st_mode)){
+            if(add_file(argfiles, path) != 0){
+                print_error("Errore nell'aggiunta del file %s alla lista\n", path);
+            }
+        }
+    }
+    closedir(dir);
+}
+
+void clear_all(linkedlist argfiles){
+    delete_list(argfiles);
+
 }
 
 
