6153aef7cfd3741a3320cc5e49fda111af28d5be
SteFerra <stefanoferralis@gmail.com>
2024-04-13 17:18:17 +0200

    Aggiornato master

--- includes/master.h
+++ includes/master.h
@@ -4,11 +4,14 @@
 
 
 #include "linked_list.h"
+#include "concurrent_queue.h"
 
 int master_worker(int argc, char *argv[]);
 
-void clear_all(linkedlist argfiles);
+void clear_all(linkedlist argfiles, concurrent_queue queue);
 
 void explore_dir(char *directory, linkedlist argfiles);
+
+int start_collector();
 
 #endif //PROGETTO_MASTER_H
