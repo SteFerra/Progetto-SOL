69885546bc67dcc11121599d479f577e657608ea
SteFerra <stefanoferralis@gmail.com>
2024-04-13 17:21:13 +0200

    Aggiornato my_signal

--- sources/my_signal.c
+++ sources/my_signal.c
@@ -1,17 +1,20 @@
 #include <pthread.h>
 #include <signal.h>
 #include <string.h>
+#include <stdio.h>
 #include "my_signal.h"
+
+volatile sig_atomic_t interrupted;
 
 void set_signal_mask(){
     sigset_t set;
     sigemptyset(&set);
-    sigaddset(&set, SIGHUP);
-    sigaddset(&set, SIGINT);
-    sigaddset(&set, SIGQUIT);
-    sigaddset(&set, SIGTERM);
-    sigaddset(&set, SIGUSR1);
-    sigaddset(&set, SIGUSR2);
+    sigaddset(&set, SIGHUP);    //codice 1
+    sigaddset(&set, SIGINT);    //codice 2
+    sigaddset(&set, SIGQUIT);   //codice 3
+    sigaddset(&set, SIGTERM);   //codice 15
+    sigaddset(&set, SIGUSR1);   //codice 10
+    sigaddset(&set, SIGUSR2);   //codice 12
     pthread_sigmask(SIG_SETMASK, &set, NULL);
 }
 
@@ -36,5 +39,6 @@ void unmask(){
 
 void termination_handler(int signum){
     interrupted = 1;
+    printf("Ricevuto segnale di terminazione\n");
 }
 
