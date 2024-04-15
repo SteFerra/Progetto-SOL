54c8d4c8a057be5b2d1c5e84cf5714ae13e63b9c
SteFerra <stefanoferralis@gmail.com>
2024-04-15 21:19:16 +0200

    Modifiche minori

--- includes/results.h
+++ includes/results.h
@@ -1,8 +1,17 @@
 #ifndef PROGETTO_RESULTS_H
 #define PROGETTO_RESULTS_H
 
+#include "util.h"
+
+typedef struct result{
+    long value;  //risultato
+    char filepath[MAX_PATH_LEN + 1];  //nome del file
+}result;
+
 typedef struct resultarray *resultarray;
 
 int init_resultarray(resultarray *array);
+int add_result(resultarray array, long value, char *filepath);
+int print_result(resultarray array);
 
 #endif //PROGETTO_RESULTS_H
