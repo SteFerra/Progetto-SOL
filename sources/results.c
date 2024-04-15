59340652b2bf96dc380f92a17859f60f245ed21d
SteFerra <stefanoferralis@gmail.com>
2024-04-15 21:19:47 +0200

    Implementata la funzione di stampa dell array dei risultati

--- sources/results.c
+++ sources/results.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <malloc.h>
+#include "util.h"
 
 typedef struct result{
     long value;  //risultato
-    char *file;  //nome del file
+    char filepath[MAX_PATH_LEN + 1];  //nome del file
 }result ;
 
 typedef struct resultarray{
@@ -29,5 +30,36 @@ int init_resultarray(resultarray **array){
     }
 
     printf("Array dei risultati inizializzato\n");
+    return 0;
+}
+
+int add_result(resultarray *array, long value, char *filepath){
+    if(array == NULL){
+        return -1;
+    }
+    if(array->size == array->capacity){
+        result *res;
+        size_t new_capacity = array->capacity * 2;
+        if((res = realloc(array->buf, new_capacity * sizeof(result))) == NULL){
+            perror("realloc");
+            return -1;
+        }
+        array->buf = res;
+        array->capacity = new_capacity;
+    }
+    array->buf[array->size].value = value;
+    strncpy(array->buf[array->size].filepath, filepath, MAX_PATH_LEN);
+    array->buf[array->size].filepath[MAX_PATH_LEN] = '\0';
+    array->size++;
+    return 0;
+}
+
+int print_result(resultarray *array){
+    if(array == NULL){
+        return -1;
+    }
+    for(int i = 0; i < array->size; i++){
+        printf("File: %s\tRisultato: %ld\n", array->buf[i].filepath, array->buf[i].value);
+    }
     return 0;
 }
