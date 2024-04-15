3cc80356bc0ff4dc3047604374f6b65b1e2dd84a
SteFerra <stefanoferralis@gmail.com>
2024-04-15 11:07:53 +0200

    Aggiunta la funzione per prendere un file dalla lista

--- includes/linked_list.h
+++ includes/linked_list.h
@@ -8,6 +8,7 @@ typedef struct linkedlist *linkedlist;
 int init_list(linkedlist* list);
 int delete_list(linkedlist list);
 int add_file(linkedlist list, char* file);
+int get_file(linkedlist list, char* file);
 
 void print_list(linkedlist list);
 
--- sources/linked_list.c
+++ sources/linked_list.c
@@ -11,35 +11,29 @@
 
 typedef struct linkedlist{
     void *string;
-    size_t dim;
-    size_t size;
-    size_t elem_size;
+    size_t dim;    //numero di elementi nella lista
+    size_t size;    //massima grandezza della lista
+    size_t elem_size;   //massima grandezza di un path
 }linkedlist;
-
-int init_list(linkedlist **list);
-int delete_list(linkedlist *list);
-int add_file(linkedlist *list, char* file);
-
 
 int init_list(linkedlist **list){
     (*list) = malloc(sizeof(linkedlist));
     if(*list == NULL){
-        goto fail;
+        free((*list)->string);
+        free(*list);
+        return -1;
     }
     (*list)->dim = 0;
     (*list)->size= LIST_SIZE;
     (*list)->elem_size = MAX_PATH_LEN + 1;
     (*list)->string = malloc(sizeof(char[(*list)->size][(*list)->elem_size]));
     if((*list)->string == NULL){
-        goto fail;
+        free((*list)->string);
+        free(*list);
+        return -1;
     }
     printf("Lista dei file inizializzata\n");
     return 0;
-
-    fail:
-    free((*list)->string);
-    free(*list);
-    return -1;
 }
 
 int delete_list(linkedlist *list){
@@ -74,7 +68,15 @@ int add_file(linkedlist *list, char* file){
     return 0;
 }
 
-/* Usato per il DEBUG
+int get_file(linkedlist *list, char* file){
+    if(list == NULL || list-> dim == 0) return -1;
+    char (*tostring)[][list->elem_size] = list->string;
+    strncpy(file, (*tostring)[--(list->dim)], list->elem_size);
+    return 0;
+}
+
+
+// Usato per il DEBUG
 void print_list(linkedlist *list){
     char (*tostring)[][list->elem_size];
     tostring = list->string;
@@ -88,4 +90,4 @@ void print_list(linkedlist *list){
     }
 }
 
- */
+
