93147225f5ddfe823edb37dfc19a9e1dba55c45e
SteFerra <stefanoferralis@gmail.com>
2024-04-21 17:54:56 +0200

    Aggiunto il comando per eliminare un thread all'arrivo di SIGUSR2

--- includes/threadpool.h
+++ includes/threadpool.h
@@ -2,14 +2,27 @@
 #define PROGETTO_THREADPOOL_H
 
 #include <pthread.h>
+#include <signal.h>
+#include "concurrent_queue.h"
+
+struct threadpool;
+
+typedef struct thread_args {
+    struct threadpool *pool;
+    concurrent_queue *queue;
+} thread_args;
 
 typedef struct threadpool {
     pthread_t *thread;  //array di thread
     int dim;    //dimensione del pool
+    int desired_threads;    //numero di thread desiderati
+    thread_args *args;
+    pthread_mutex_t mutex;
 } threadpool;
 
 int init_threadpool(threadpool *pool, int nthread, void* (*task)(void *), void* arg);
 int create_thread(threadpool *pool, void* (*task)(void *), void* arg);
+int delete_thread(threadpool *pool, concurrent_queue *queue);
 int wait_threadpool(threadpool *pool);
 int delete_threadpool(threadpool *pool);
 
--- sources/threadpool.c
+++ sources/threadpool.c
@@ -10,17 +10,24 @@
 int init_threadpool(threadpool *pool, int nthread, void* (*task)(void *), void* arg){
 
     pool->dim = 0;
+    pool->desired_threads = nthread;
 
     if((pool->thread = malloc(sizeof(pthread_t) * nthread)) == NULL){
         return -1;
     }
 
+    pool->args = malloc(sizeof(thread_args));
+    pool->args->pool = pool;
+    pool->args->queue = (concurrent_queue *)arg;
+
     for(int i = 0; i < nthread; i++){
-        if(pthread_create(&(pool)->thread[i], NULL, task, arg) != 0){
+        if(pthread_create(&(pool)->thread[i], NULL, task, pool->args) != 0){
             return -1;
         }
         ++pool->dim;
     }
+
+    pthread_mutex_init(&pool->mutex, NULL);
     return 0;
 }
 
@@ -28,14 +35,46 @@ int create_thread(threadpool *pool, void* (*task)(void *), void* arg){
     if(pool == NULL){
         return -1;
     }
-    if(pthread_create(&pool->thread[pool->dim], NULL, task, arg) != 0){
+
+    //creo un thread nel pool
+    //incremento la dimensione del pool
+
+    pool->args = malloc(sizeof(thread_args));
+    pool->args->pool = pool;
+    pool->args->queue = (concurrent_queue *)arg;
+
+    if(pthread_create(&(pool)->thread[pool->dim], NULL, task, pool->args) != 0){
         return -1;
     }
+
     ++pool->dim;
-    printf("Thread creato\n");
-    printf("Numero di thread: %d\n", pool->dim);
+    ++pool->desired_threads;
+    printf("dim: %d\n", pool->dim);
+
     return 0;
 }
+
+int delete_thread(threadpool *pool, concurrent_queue *queue){
+    if(pool == NULL){
+        return -1;
+    }
+
+    //se il pool è vuoto non posso eliminare thread
+    if(pool->dim == 1 || pool == NULL){
+        return -1;
+    }
+
+    //elimino un thread dal pool
+    //decremento la dimensione del pool
+
+    --pool->desired_threads;
+
+    printf("dim: %d\n", pool->dim);
+
+    return 0;
+}
+
+
 
 int wait_threadpool(threadpool *pool){
     for(int i = 0; i < pool->dim; i++){
@@ -44,5 +83,27 @@ int wait_threadpool(threadpool *pool){
         }
     }
     return 0;
+}
+
+int delete_threadpool(threadpool *pool){
+    if(pool == NULL){
+        return -1;
+    }
+    free(pool->args);
+    free(pool->thread);
+    return 0;
+}
+
+int get_thread_index(threadpool *pool){
+    if(pool == NULL){
+        return -1;
+    }
+    pthread_t self = pthread_self();
+    for (int i = 0; i < pool->dim; i++) {
+        if (pthread_equal(pool->thread[i], self)) {
+            return i;
+        }
+    }
+    return -1;  // Return -1 if the current thread is not found in the pool
 }
 
